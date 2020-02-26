5010dec4fdf37e2d82d9c48d40c9819c77b8c964
Pablo <p.rosadog@alumnos.upm.es>
2020-02-26 13:38:40 +0100

    Changes in main code

--- main/bluetooth.c
+++ main/bluetooth.c
@@ -1,33 +1,49 @@
 //#include all the libraries that I wiil need to implement bluetooth
-
+//All this code must go to the main function.
 
 int getCount(){
 
-	int count = 0;
+	int cont = 0;
+
+	//init ble scan
+
+	Serial.begin(115200);
+  	Serial.println("Scanning...");
+
+	BLEDevice::init("");
+	pBLEScan = BLEDevice::getScan(); //create new scan
+	pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+	pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+	pBLEScan->setInterval(100);
+	pBLEScan->setWindow(99);  // less or equal setInterval value
 
 	//In this part we are going to put everything related with bluetooth scan using ble. 
 	//Should confirm if We can do the scan usig classic bluetooth.
 
-	count = devices.found(); 		//devices.found is a function of the library related to ble.
+	//count = devices.found(); 		//devices.found is a function of the library related to ble.
 
-	return count;
+	//Scan part
+
+	BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  	cont = foundDevices.getCount();
+
+	return cont;
 }
 
 /*n this case, we are going to use the esp32 as a server and the phone as client. The esp32 will send all the data to the client(phone)*/
 
-void connect(){
+int connect(){
 
-	string UUID;					//this is the identifier of the service.
-	bool connected = false;			//used as a control variable.
+	//string UUID;					//this is the identifier of the service.
+	int connected = 0;			//used as a control variable.
 
-	//In this part, everything related to create a ble server. Probably, we should use characteristics or something like that.
-
+	//In this part, everything related to create a ble server. Probably, we should use characteristics or something like that.  rerturn connected;
 }
 
 //this function is used to send all the information take it by the sensors and send it to the phone.
 
-void sendData(int temperature, int lux, int count, uint16_t CO2){
-
+//void sendData(int temperature, int lux, int count, uint16_t CO2){
+/*
 	send(temperature, lux, count, CO2);
 
 	//or
@@ -35,7 +51,7 @@ void sendData(int temperature, int lux, int count, uint16_t CO2){
 	send (temperature);
 	send (lux);
 	send (count);
-	send (CO2);
+	send (CO2);*/
 
 	//How to send data depends of how phone must recieve it.
-}
+//}
--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,19 @@
-#include "temperatura.c"
-#include "luz.c"
-#include "CO2.c"
-#include "bluetooth.c"
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <Wire.h>
+#include <SparkFun_SCD30_Arduino_Library.h>
+
+//Due to problems with libraries, all the code must be here. I don't really like it, but xD. For that reason, all the .c file must disappear.
 
 #define N 12
 
-void main (){
+BLEScan* pBLEScan;
+
+SCD30 CO2;
+
+void Setup (){
 
 	//parametros necesarios para inicializar esp32
 	int orden = 0;
@@ -13,65 +21,65 @@ void main (){
 	int lux = 0;
 	uint16_t C02 = 0x0000;
 	int personas = 0;
-	char MACs [][N];				//el valor de la x debería ser variable (necesario ver como se hace) OJO AL STACK
+  int orden = 0;
 
-	while (1){
+}
 
-		//por UART recibe la información del robot de gobierno 
+void loop (){
+      //por UART recibe la información del robot de gobierno 
 
-		orden = UARTGobierno();
+    orden = UARTGobierno();
 
-		/*al tener un total de 4 clases sin contar micrófono podemos comprender entre 1 y 5 los valores siendo
-			1) solicitud de temperatura
-			2) Solicitud de luz
-			3) solicitud de CO2
-			4) solicitud de nuúmero de personas
-			5) Todo
-		*/
-		//en este punto podemos hacerlo con todo ifs o podemos considerar la opción de un switch dependiendo de lo más óptimo
-		
-		//opcion 1:
-		if (orden != 0){
-			if (orden == 1)
-				temperatura = getTemperatura ();
-			else if (orden == 2)
-				lux = getLux();
-			else if (orden == 3)
-				CO2 = getCO2();
-			else if (orden == 4)
-				personas = count(MACs[][N]);
-			else if (orden == 5){
-				temperatura = getTemperatura();
-				lux = getLux ();
-				CO2 = getCO2();
-				personas = count (MACs [][N]);
-			}
-		}
+    /*al tener un total de 4 clases sin contar micrófono podemos comprender entre 1 y 5 los valores siendo
+      1) solicitud de temperatura
+      2) Solicitud de luz
+      3) solicitud de CO2
+      4) solicitud de nuúmero de personas
+      5) Todo
+    */
+    //en este punto podemos hacerlo con todo ifs o podemos considerar la opción de un switch dependiendo de lo más óptimo
+    
+    //opcion 1:
+    if (orden != 0){
+      if (orden == 1)
+        temperatura = getTemperatura ();
+      else if (orden == 2)
+        lux = getLux();
+      else if (orden == 3)
+        CO2 = getCO2();
+      else if (orden == 4)
+        personas = count(MACs[][N]);
+      else if (orden == 5){
+        temperatura = getTemperatura();
+        lux = getLux ();
+        CO2 = getCO2();
+        personas = count (MACs [][N]);
+      }
+    }
 
-		//opcion 2
-		if (orden != 0){
-			do{
-				switch(orden){
-					case 1: temperatura = getTemperatura();
-							break;
-					case 2: lux = getLux();
-							break;
-					case 3: CO2 = getCO2();
-							break;
-					case 4: personas = count (MACs[][N]);
-							break;
-					case 5: temperatura = getTemperatura();
-							lux = getLux();
-							CO2 = getCO2();
-							personas = count (MACs[][N]); 
-							break;
-				}
-			}while (orden != 6);
-		}
+    //opcion 2
+    if (orden != 0){
+      do{
+        switch(orden){
+          case 1: temperatura = getTemperatura();
+              break;
+          case 2: lux = getLux();
+              break;
+          case 3: CO2 = getCO2();
+              break;
+          case 4: personas = count (MACs[][N]);
+              break;
+          case 5: temperatura = getTemperatura();
+              lux = getLux();
+              CO2 = getCO2();
+              personas = count (MACs[][N]); 
+              break;
+        }
+      }while (orden != 6);
+    }
 
-		UARTGobierno ("Temperatura: %f ºC", temperatura);
-		UARTGobierno ("Luz: %d lux", lux);
-		UARTGobierno ("CO2: %d ppm", CO2);
-		UARTGobierno ("Numero de personas: %d personas", personas);
-	}
+    UARTGobierno ("Temperatura: %f ºC", temperatura);
+    UARTGobierno ("Luz: %d lux", lux);
+    UARTGobierno ("CO2: %d ppm", CO2);
+    UARTGobierno ("Numero de personas: %d personas", personas);
 }
