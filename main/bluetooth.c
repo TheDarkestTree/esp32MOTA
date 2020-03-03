6563c8c18fe3fcac7875b70de30e46f7fcd7794b
Pablo <p.rosadog@alumnos.upm.es>
2020-03-03 11:49:04 +0100

    Main changed

--- main/bluetooth.c
+++ main/bluetooth.c
@@ -54,3 +54,96 @@ int connect(){
 
 	//How to send data depends of how phone must recieve it.
 //}
+/*#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+char hola = 'h';
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "62e0fa9c-7133-4027-ba76-181c47d64b23"
+#define CHARACTERISTIC_UUID "525e57f2-eeed-4712-96c1-2a68c6248fc4"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("Device connected");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("DEvice Disconnected");
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+    // notify changed value
+    if (deviceConnected) {
+        //pCharacteristic->setValue((uint8_t*)&value, 5);
+        //pCharacteristic->notify();
+        //pCharacteristic->setValue((uint8_t*)&hola, 2);
+        pCharacteristic->setValue("Hola mi amor");
+        pCharacteristic->notify();
+        Serial.println(hola);
+        //value++;
+        delay(200); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+        
+    }
+}*/
--- main/main.ino
+++ main/main.ino
@@ -4,18 +4,52 @@
 #include <BLEAdvertisedDevice.h>
 #include <Wire.h>
 #include <SparkFun_SCD30_Arduino_Library.h>
-
+#include <BLEServer.h>
+#include <BLE2902.h>
 
 //Due to problems with libraries, all the code must be here. I don't really like it, but xD. For that reason, all the .c file must disappear.
 
-#define pinT 6
-#define pinL 7
+
+//This pins are denifed as GPIO not like phisic number.
+#define pinT 32
+#define pinL 33
+#define pinRX 16
+#define pinTX 17
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+float temperatura = 0.0;
+int lux = 0;
+uint16_t C02 = 0x0000;
+int personas = 0;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+int scanTime = 5;
 
 
 BLEScan* pBLEScan;
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
 
 SCD30 CO2sensor;
 
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
 
 //Temperature sensor control function
 
@@ -32,7 +66,6 @@ float getTemperature(){
 	return temperature;
 }
 
-
 //Fotoresistor control function
 
 int getLux (){
@@ -43,6 +76,8 @@ int getLux (){
 	
 	return lux;
 }
+
+//CO2 sensor function
 
 uint16_t getCO2(){
 		
@@ -58,73 +93,109 @@ uint16_t getCO2(){
 	return CO2value;		
 }
 
+//Bluetooth scan
 
+int getCount(){
+
+	int cont = 0;
+
+	//init ble scan
+
+	Serial.begin(115200);
+  	Serial.println("Scanning...");
+
+	BLEDevice::init("");
+	pBLEScan = BLEDevice::getScan(); //create new scan
+	pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+	pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+	pBLEScan->setInterval(100);
+	pBLEScan->setWindow(99);  // less or equal setInterval value
+
+	//In this part we are going to put everything related with bluetooth scan using ble. 
+	//Should confirm if We can do the scan usig classic bluetooth.
+
+	//count = devices.found(); 		//devices.found is a function of the library related to ble.
+
+	//Scan part
+
+	BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  cont = foundDevices.getCount();
+  pBLEScan->clearResults();
+	return cont;
+}
+
+//Bluetooth sever to send data
+
+void sendData(float temperatura, int lux, uint16_t CO2, int personas){
+
+	if (deviceConnected) {
+        Serial.println(getCount());
+        personas = getCount();
+        pCharacteristic->setValue(personas);
+        
+        pCharacteristic->notify();
+        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
 
 void Setup (){
 
 	//parametros necesarios para inicializar esp32
-	float temperatura = 0.0;
-	int lux = 0;
-	uint16_t C02 = 0x0000;
-	int personas = 0;
+	Serial.begin(115200);
+	Serial2.begin(115200, SERIAL_8N1, pinRX, pinTX);
+	// Create the BLE Device
+	BLEDevice::init("ESP32");
+	// Create the BLE Server
+	pServer = BLEDevice::createServer();
+	pServer->setCallbacks(new MyServerCallbacks());
+	// Create the BLE Service
+	BLEService *pService = pServer->createService(SERVICE_UUID);
+	// Create a BLE Characteristic
+	pCharacteristic = pService->createCharacteristic(
+	                    CHARACTERISTIC_UUID,
+	                    BLECharacteristic::PROPERTY_READ   |
+	                    BLECharacteristic::PROPERTY_WRITE  |
+	                    BLECharacteristic::PROPERTY_NOTIFY |
+	                    BLECharacteristic::PROPERTY_INDICATE
+	                  );
+	// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+	// Create a BLE Descriptor
+	pCharacteristic->addDescriptor(new BLE2902());
+
+	pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+	// Start the service
+	pService->start();
+	// Start advertising
+	BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+	pAdvertising->addServiceUUID(SERVICE_UUID);
+	pAdvertising->setScanResponse(false);
+	pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+	BLEDevice::startAdvertising();
+	Serial.println("Waiting a client connection to notify...");
+	
 }
 
 void loop (){
-      //por UART recibe la información del robot de gobierno 
+      
+    //in this case, i should comprovate if the robot is waiting for the data. I must ask to the teacher.
 
-    int orden = 0;
-    orden = UARTGobierno();
-
-    /*al tener un total de 4 clases sin contar micrófono podemos comprender entre 1 y 5 los valores siendo
-      1) solicitud de temperatura
-      2) Solicitud de luz
-      3) solicitud de CO2
-      4) solicitud de nuúmero de personas
-      5) Todo
-    */
-    //en este punto podemos hacerlo con todo ifs o podemos considerar la opción de un switch dependiendo de lo más óptimo
     
-    //opcion 1:
-    if (orden != 0){
-      if (orden == 1)
-        temperatura = getTemperatura ();
-      else if (orden == 2)
-        lux = getLux();
-      else if (orden == 3)
-        CO2 = getCO2();
-      else if (orden == 4)
-        personas = count(MACs[][N]);
-      else if (orden == 5){
-        temperatura = getTemperatura();
-        lux = getLux ();
-        CO2 = getCO2();
-        personas = count (MACs [][N]);
-      }
-    }
-
-    //opcion 2
-    if (orden != 0){
-      do{
-        switch(orden){
-          case 1: temperatura = getTemperatura();
-              break;
-          case 2: lux = getLux();
-              break;
-          case 3: CO2 = getCO2();
-              break;
-          case 4: personas = count (MACs[][N]);
-              break;
-          case 5: temperatura = getTemperatura();
-              lux = getLux();
-              CO2 = getCO2();
-              personas = count (MACs[][N]); 
-              break;
-        }
-      }while (orden != 6);
-    }
-
-    UARTGobierno ("Temperatura: %f ºC", temperatura);
-    UARTGobierno ("Luz: %d lux", lux);
-    UARTGobierno ("CO2: %d ppm", CO2);
-    UARTGobierno ("Numero de personas: %d personas", personas);
+  
 }
