c019fdce58670e84ed3005f8a1c3133a1a3bb531
Pablo <p.rosadog@alumnos.upm.es>
2020-03-06 18:51:54 +0100

    Sending data completed.

--- main/main.ino
+++ main/main.ino
@@ -19,7 +19,7 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
-float temperatura = 0.0;
+int temperature = 15;
 int lux = 0;
 uint16_t C02 = 0x0000;
 int personas = 0;
@@ -27,6 +27,7 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
 int scanTime = 5;
+int option = 0;
 
 
 BLEScan* pBLEScan;
@@ -35,11 +36,15 @@ BLECharacteristic* pCharacteristic = NULL;
 
 SCD30 CO2sensor;
 
+//Scanner
+
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
     }
 };
+
+//Server
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -53,89 +58,89 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
 //Temperature sensor control function
 
-float getTemperature(){
+int getTemperature(){
+  
+  int valorSensor = 0;
+  int voltajeSal = 0;
 
-	float temperature = 0.0;
-	float valorSensor = 0.0;
-	float voltajeSal = 0.0;
+  //valorSensor = analogRead (pinT);      //pinT dependera del pin en el que se encuentre el sensor. analogRead funcion de la libreria de arduino.
+  voltajeSal = (valorSensor * 5000)/ 1024;  // 5000 no comprendo de donde sale. 1024 es el correspondiente en arduino de 5v.
+  temperature = 15;//voltajeSal / 10;        //en el caso del sensor LM35 son 10mV para convertirlo en ºC.
 
-	valorSensor = analogRead (pinT); 			//pinT dependera del pin en el que se encuentre el sensor. analogRead funcion de la libreria de arduino.
-	voltajeSal = (valorSensor * 5000)/ 1024;	// 5000 no comprendo de donde sale. 1024 es el correspondiente en arduino de 5v.
-	temperature = voltajeSal / 10;				//en el caso del sensor LM35 son 10mV para convertirlo en ºC.
-
-	return temperature;
+  return temperature;
 }
 
 //Fotoresistor control function
 
 int getLux (){
 
-	int lux = 0;
-	
-	lux = analogRead (pinL);			//In the case of this sensor, we don't need to use any formula. It's enough reading data of the pin.
-	
-	return lux;
+  int lux = 0;
+  
+ // lux = analogRead (pinL);      //In the case of this sensor, we don't need to use any formula. It's enough reading data of the pin.
+  
+  return lux;
 }
 
 //CO2 sensor function
 
 uint16_t getCO2(){
-		
-	uint16_t CO2value = 0x0000;		//son un total de 16 bits
+    
+  uint16_t CO2value = 0x0000;   //son un total de 16 bits
 
-	Wire.begin();
+  /*Wire.begin();
+  if (CO2sensor.dataAvailable())
+    CO2value = CO2sensor.getCO2();*/
 
-	if (CO2sensor.dataAvailable()){
-
-		CO2value = CO2sensor.getCO2();
-	}
-
-	return CO2value;		
+  return CO2value;    
 }
 
 //Bluetooth scan
 
 int getCount(){
 
-	int cont = 0;
+  int cont = 0;
 
-	//init ble scan
+  //init ble scan
+    Serial.println("Scanning...");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
 
-	Serial.begin(115200);
-  	Serial.println("Scanning...");
+  //In this part we are going to put everything related with bluetooth scan using ble. 
+  //Should confirm if We can do the scan usig classic bluetooth.
 
-	BLEDevice::init("");
-	pBLEScan = BLEDevice::getScan(); //create new scan
-	pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-	pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
-	pBLEScan->setInterval(100);
-	pBLEScan->setWindow(99);  // less or equal setInterval value
+  //count = devices.found();    //devices.found is a function of the library related to ble.
 
-	//In this part we are going to put everything related with bluetooth scan using ble. 
-	//Should confirm if We can do the scan usig classic bluetooth.
+  //Scan part
 
-	//count = devices.found(); 		//devices.found is a function of the library related to ble.
-
-	//Scan part
-
-	BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  cont = foundDevices.getCount();
-  pBLEScan->clearResults();
-	return cont;
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+    cont = foundDevices.getCount();
+    pBLEScan->clearResults();
+    Serial.println("Aqui llego");
+  return cont;
 }
 
 //Bluetooth sever to send data
 
-void sendData(float temperatura, int lux, uint16_t CO2, int personas){
+void sendDataBLE(){
 
-	if (deviceConnected) {
-        Serial.println(getCount());
-        personas = getCount();
-        pCharacteristic->setValue(personas);
-        
+  String data;
+
+  if (deviceConnected) {
+        data = "/" ;
+        data = data + getTemperature();
+        data = data + "/" + getLux();
+        data = data + "/" + getCO2();
+        data = data + "/" + getCount();
+        Serial.println("Data: " + data);
+        pCharacteristic->setValue(data.c_str());
+        Serial.println("Aqui también");
         pCharacteristic->notify();
-        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+        delay(600); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
+
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
         delay(500); // give the bluetooth stack the chance to get things ready
@@ -143,6 +148,7 @@ void sendData(float temperatura, int lux, uint16_t CO2, int personas){
         Serial.println("start advertising");
         oldDeviceConnected = deviceConnected;
     }
+
     // connecting
     if (deviceConnected && !oldDeviceConnected) {
         // do stuff here on connecting
@@ -150,52 +156,72 @@ void sendData(float temperatura, int lux, uint16_t CO2, int personas){
     }
 }
 
-void Setup (){
+void setup (){
 
-	//parametros necesarios para inicializar esp32
-	Serial.begin(115200);
-	Serial2.begin(115200, SERIAL_8N1, pinRX, pinTX);
-	// Create the BLE Device
-	BLEDevice::init("ESP32");
-	// Create the BLE Server
-	pServer = BLEDevice::createServer();
-	pServer->setCallbacks(new MyServerCallbacks());
-	// Create the BLE Service
-	BLEService *pService = pServer->createService(SERVICE_UUID);
-	// Create a BLE Characteristic
-	pCharacteristic = pService->createCharacteristic(
-	                    CHARACTERISTIC_UUID,
-	                    BLECharacteristic::PROPERTY_READ   |
-	                    BLECharacteristic::PROPERTY_WRITE  |
-	                    BLECharacteristic::PROPERTY_NOTIFY |
-	                    BLECharacteristic::PROPERTY_INDICATE
-	                  );
-	// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-	// Create a BLE Descriptor
-	pCharacteristic->addDescriptor(new BLE2902());
+  //parametros necesarios para inicializar esp32
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, pinRX, pinTX);
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
 
-	pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan = BLEDevice::getScan(); //create new scan
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);
 
-	// Start the service
-	pService->start();
-	// Start advertising
-	BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-	pAdvertising->addServiceUUID(SERVICE_UUID);
-	pAdvertising->setScanResponse(false);
-	pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
-	BLEDevice::startAdvertising();
-	Serial.println("Waiting a client connection to notify...");
-	
+  // Start the service
+  pService->start();
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+  
 }
 
-void loop (){
-      
-    //in this case, i should comprovate if the robot is waiting for the data. I must ask to the teacher.
+void loop (void){
 
-    
-  
+    String prueba;
+    //option = Serial2.read();
+
+    sendDataBLE();
+    /*if (option != 0){
+   		switch(option){
+			case 1: Serial2.write((int)getTemperature());
+		  		break;
+		  	case 2: Serial2.write(getLux());
+		  		break;
+		  	case 3: Serial2.write(getCO2());
+		  		break;
+		  	case 4: Serial2.write(getCount());
+		  		break;
+		  	case 5: //In this case we have two options. Send all the information in only one string or sending one by one
+		  			prueba = "Personas: " + getCount();
+		  			prueba = prueba + " CO2: " + getCO2();
+		  			prueba = prueba + " Luz: " + getLux();
+		  			prueba = prueba + " Temperature" + getTemperature();
+		  			Serial2.write(prueba.c_str());
+		  		break;
+   		}
+   	}*/
+   	delay(40);
 }
